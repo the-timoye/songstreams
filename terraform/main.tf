e6226ecf154373d79912c07e0faf8cfc17e4b9cf
the-timoye <thetimoye@gmail.com>
2022-05-05 22:48:42 +0100

    config(terraform): specify provider, variables

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">=1.0"
+  backend "local" {}
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+}
