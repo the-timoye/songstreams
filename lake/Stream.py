1ad1cf5d796861863a9e44f46e9ab8d067b54cbe
the-timoye <thetimoye@gmail.com>
2022-06-20 15:14:38 +0100

    fix(streams): use negative check incase of additional columns

--- lake/Stream.py
+++ lake/Stream.py
@@ -35,11 +35,11 @@ class Stream():
     def write(stream, path, format, topic):
         response = stream.writeStream\
             .format(format)\
-            .partitionBy('__state', '__month', '__day_of_week')\
+            .partitionBy('__year', '__month',)\
             .option("header", True)\
             .option("path", f'{path}/{topic}')\
             .option("checkpointLocation", f'{path}/checkpoint/{topic}')\
-            .trigger(processingTime="120 seconds")\
+            .trigger(processingTime="150 seconds")\
             .outputMode("append")
         return response
 
@@ -53,23 +53,24 @@ class Stream():
                         .select("data.*")
                         )
         raw_response = raw_response.withColumn("ts", (col("ts")/1000).cast("timestamp"))\
+            .withColumn("__year", year(col("ts")))\
             .withColumn("year", year(col("ts")))\
             .withColumn("month", month(col("ts")))\
             .withColumn("abs_date", date_trunc("dd", col("ts")))\
             .withColumn("__month", month(col("ts")))\
             .withColumn("hour", hour(col("ts")))\
-            .withColumn("__state", (col("state")))\
             .withColumn("day", dayofmonth(col("ts")))\
             .withColumn("day_of_week", dayofweek(col('ts')))\
-            .withColumn("__day_of_week", dayofweek(col('ts')))\
             .withColumn("is_weekend", dayofweek(col("ts")).isin(1, 7).cast("boolean"))
 
         clean_response = rename_columns(raw_response, new_column_names)
-        if topic in ["listen_events", "page_view_events"]:
-            clean_response = (clean_response
-                              .withColumn("song", string_decode("song"))
-                              .withColumn("artist", string_decode("artist"))
-                              )
+        clean_response = clean_response.withColumn(
+            "user_agent", string_decode("user_agent"))
+        if topic not in ["auth_events"]:
+            clean_response = clean_response\
+                .withColumn("song", string_decode("song"))\
+                .withColumn("artist", string_decode("artist"))
+
         clean_response = clean_response\
             .na.fill(value=-404)\
             .na.fill(value='N/A')
