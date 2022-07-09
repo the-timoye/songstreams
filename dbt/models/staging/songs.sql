aeaebbed91759b093e41569536459859b1c0a131
the-timoye <thetimoye@gmail.com>
2022-07-10 00:31:10 +0100

    chore(denormalize): eliminate artists table

--- dbt/models/staging/songs.sql
+++ dbt/models/staging/songs.sql
@@ -1,32 +1,25 @@
 WITH listen_songs AS (
     SELECT 
         DISTINCT le.song as title,
-        le.artist AS artist_name,
-        ar._id AS artist_id
+        le.artist AS artist_name
     FROM {{source('dev', 'listen_events')}} AS le
-    JOIN {{ ref('artists') }} AS ar
-    ON ar.name = le.artist
-    WHERE song <> 'N/A'
 ),
 page_view_songs AS (
     SELECT 
         DISTINCT song as title,
-        le.artist AS artist_name,
-        ar._id AS artist_id
+        le.artist AS artist_name
     FROM {{ source('dev', 'page_view_events') }} AS le
-    JOIN {{ ref('artists') }} AS ar
-    ON ar.name = le.artist
-    WHERE song <> 'N/A'
 ),
 all_songs AS (
-    SELECT * FROM listen_songs
+    SELECT * 
+    FROM listen_songs
     UNION
     SELECT * FROM page_view_songs
 )
 
 SELECT {{ dbt_utils.surrogate_key(
     'title',
-    'artist_id'
-  ) }} AS _id, title, artist_id, artist_name
+    'artist_name'
+  ) }} AS _id, title, artist_name
 FROM all_songs
 
